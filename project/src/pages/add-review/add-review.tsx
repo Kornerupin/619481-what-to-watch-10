aca859ee316b902b767209d4c81261c1e426dfe9
Kornerupin <kornerupin@yandex.ru>
2022-08-28 13:26:00 +0300

    Fix bugs with evt & no-spread

--- project/src/pages/add-review/add-review.tsx
+++ project/src/pages/add-review/add-review.tsx
@@ -3,7 +3,6 @@ import {Navigate, useParams} from 'react-router-dom';
 import {AppRoute} from '../../const';
 import Logo from '../../components/logo/logo';
 import {ChangeEvent, useState} from 'react';
-import * as events from 'events';
 
 type AddReviewProps = {
   listData: Film[],
@@ -15,17 +14,17 @@ const AddReview = ({listData}: AddReviewProps) => {
   const film = listData[id];
 
   const [stateData, setStateData] = useState({
-    rating: '0',
-    text: '',
+    rating: '5',
+    'review-text': 'test',
   });
 
-  const filedInputHandler = (evt: ChangeEvent<HTMLTextAreaElement>) => {
+  const filedInputHandler = (evt: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>) => {
     const {name, value} = evt.target;
     setStateData({
       ...stateData,
       [name]: value,
     });
-  }
+  };
 
   if (!film) {
     return <Navigate to={AppRoute.Main} />;
@@ -75,40 +74,40 @@ const AddReview = ({listData}: AddReviewProps) => {
         <form action='src/pages/add-review/add-review#' className='add-review__form'>
           <div className='rating'>
             <div className='rating__stars'>
-              <input className='rating__input' id='star-10' type='radio' name='rating' value='10' {stateData.rating === '10' ? 'checked' : ''} onInput={setStateData} />
+              <input className='rating__input' id='star-10' type='radio' name='rating' value='10' checked={stateData.rating === '10'} onChange={filedInputHandler} />
               <label className='rating__label' htmlFor='star-10'>Rating 10</label>
 
-              <input className='rating__input' id='star-9' type='radio' name='rating' value='9' {stateData.rating === '9' ? 'checked' : ''} onInput={setStateData} />
+              <input className='rating__input' id='star-9' type='radio' name='rating' value='9' checked={stateData.rating === '9'} onChange={filedInputHandler} />
               <label className='rating__label' htmlFor='star-9'>Rating 9</label>
 
-              <input className='rating__input' id='star-8' type='radio' name='rating' value='8' {stateData.rating === '8' ? 'checked' : ''} onInput={setStateData} />
+              <input className='rating__input' id='star-8' type='radio' name='rating' value='8' checked={stateData.rating === '8'} onChange={filedInputHandler} />
               <label className='rating__label' htmlFor='star-8'>Rating 8</label>
 
-              <input className='rating__input' id='star-7' type='radio' name='rating' value='7' {stateData.rating === '7' ? 'checked' : ''} onInput={setStateData} />
+              <input className='rating__input' id='star-7' type='radio' name='rating' value='7' checked={stateData.rating === '7'} onChange={filedInputHandler} />
               <label className='rating__label' htmlFor='star-7'>Rating 7</label>
 
-              <input className='rating__input' id='star-6' type='radio' name='rating' value='6' {stateData.rating === '6' ? 'checked' : ''} onInput={setStateData} />
+              <input className='rating__input' id='star-6' type='radio' name='rating' value='6' checked={stateData.rating === '6'} onChange={filedInputHandler} />
               <label className='rating__label' htmlFor='star-6'>Rating 6</label>
 
-              <input className='rating__input' id='star-5' type='radio' name='rating' value='5' {stateData.rating === '5' ? 'checked' : ''} onInput={setStateData} />
+              <input className='rating__input' id='star-5' type='radio' name='rating' value='5' checked={stateData.rating === '5'} onChange={filedInputHandler} />
               <label className='rating__label' htmlFor='star-5'>Rating 5</label>
 
-              <input className='rating__input' id='star-4' type='radio' name='rating' value='4' {stateData.rating === '4' ? 'checked' : ''} onInput={setStateData} />
+              <input className='rating__input' id='star-4' type='radio' name='rating' value='4' checked={stateData.rating === '4'} onChange={filedInputHandler} />
               <label className='rating__label' htmlFor='star-4'>Rating 4</label>
 
-              <input className='rating__input' id='star-3' type='radio' name='rating' value='3' {stateData.rating === '3' ? 'checked' : ''} onInput={setStateData} />
+              <input className='rating__input' id='star-3' type='radio' name='rating' value='3' checked={stateData.rating === '3'} onChange={filedInputHandler} />
               <label className='rating__label' htmlFor='star-3'>Rating 3</label>
 
-              <input className='rating__input' id='star-2' type='radio' name='rating' value='2' {stateData.rating === '2' ? 'checked' : ''} onInput={setStateData} />
+              <input className='rating__input' id='star-2' type='radio' name='rating' value='2' checked={stateData.rating === '2'} onChange={filedInputHandler} />
               <label className='rating__label' htmlFor='star-2'>Rating 2</label>
 
-              <input className='rating__input' id='star-1' type='radio' name='rating' value='1' {stateData.rating === '1' ? 'checked' : ''} onInput={setStateData} />
+              <input className='rating__input' id='star-1' type='radio' name='rating' value='1' checked={stateData.rating === '1'} onChange={filedInputHandler} />
               <label className='rating__label' htmlFor='star-1'>Rating 1</label>
             </div>
           </div>
 
           <div className='add-review__text'>
-            <textarea className='add-review__textarea' name='review-text' id='review-text' placeholder='Review text' onInput={setStateData}>{stateData.text}</textarea>
+            <textarea className='add-review__textarea' name='review-text' id='review-text' placeholder='Review text' value={stateData['review-text']} onChange={filedInputHandler}></textarea>
             <div className='add-review__submit'>
               <button className='add-review__btn' type='submit'>Post</button>
             </div>
